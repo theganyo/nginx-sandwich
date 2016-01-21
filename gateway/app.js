d1552c2c43ff435d6adece52f632147eeb46a320
Scott Ganyo <scott@ganyo.com>
2016-01-21 14:46:34 -0800

    add internal target

--- gateway/app.js
+++ gateway/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const http = require('http')
 const request = require('request')
 
-const outbound_nginx = process.env.outbound
+const outbound_nginx = process.env.ROUTER_SERVICE_HOST
 
 const app = express();
 app.set('port', process.env.PORT || 10010)
@@ -13,7 +13,7 @@ app.get('/', (req, res, next) => {
 
   // back to nginx 
   const options = {
-    url: outbound_nginx,
+    url: `${outbound_nginx}:8081`,
     headers: {
       'Host': req.get('host')
     }
